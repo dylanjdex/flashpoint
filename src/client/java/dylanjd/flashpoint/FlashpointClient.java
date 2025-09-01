fcad361fb2f4fc4640d38ff883abfdd88850f337
dylanjd <123621188+dylanjdex@users.noreply.github.com>
2025-09-01 15:48:11 -0400

    init

---
+++ src/client/java/dylanjd/flashpoint/FlashpointClient.java
@@ -0,0 +1,10 @@
+package dylanjd.flashpoint;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FlashpointClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
